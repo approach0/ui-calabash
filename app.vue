1e8acda1aa5c74f2b6790c3a0f1fded9958395b5
t-k- <clock126@126.com>
2020-10-29 02:27:37 +0800

    fix job box responsive layout.

--- app.vue
+++ app.vue
@@ -4,11 +4,10 @@
       <h2>Calabash Panel</h2>
     </div>
 
-    <div class="p-col-12 p-md-8 p-lg-6 p-input-icon-left">
+    <div class="p-col-12 p-md-8 p-lg-6 p-input-icon-left p-d-flex p-ai-center">
       <i class="pi pi-caret-right p-pl-3"></i>
-      <InputText type="text" v-model="input_job" style="width: 70%;"
+      <InputText type="text" v-model="input_job" style="flex-grow: 1;"
        placeholder="Run job" class="p-inputtext-sm p-m-2"/>
-
       <SplitButton label="Run" :model="run_btn_model" @click="runJob()"></SplitButton>
     </div>
 
